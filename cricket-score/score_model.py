792dace6b3dab5294c52170affe8e310a26c9f7d
paikeaiti <paikeaiti@gmail.com>
2014-01-12 22:42:38 +1300

    Initial commit. Test of creating a class.

---
+++ cricket-score/score_model.py
@@ -0,0 +1,14 @@
+"""
+"""
+
+
+class Team(object):
+    """
+    A cricket team, with a minimum of 12 players. There may be more than 12,
+    but during a game only a maximum of 12 will participate.
+    """
+
+    def __init__(self, params):
+        """
+        Constructor
+        """
