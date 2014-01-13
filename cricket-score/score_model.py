5a9eb18e39ebd5b87fc15a90a00db530560b3af7
paikeaiti <paikeaiti@gmail.com>
2014-01-13 22:32:21 +1300

    Initial addition of the ball. Continued addition of tests for Team and Player

--- cricket-score/score_model.py
+++ cricket-score/score_model.py
@@ -12,9 +12,18 @@ class Team(object):
         """
         Create a new team, with the given name
         """
-        if not name:
-            name = ''
         self.name = name
+        self.players = list()
+
+    @property
+    def num_players(self):
+        return len(self.players)
+
+    def add_player(self, player):
+        self.players.append(player)
+
+    def remove_player(self,player):
+        self.players.remove(player)
 
 
 class Player(object):
@@ -23,5 +32,16 @@ class Player(object):
     both). Their statistics will be kept here.
     """
 
-    def __init__(self, name):
+    def __init__(self, name, ident=None):
         self.name = name
+        self.identifier = ident
+
+
+class Ball(object):
+    """
+    The core interaction between the bowler and batter
+    Due to 'illegal' bowling there may be more than one actual ball bowled
+    for each ball. These extras need to be recorded as well,"""
+    def __init__(self,player,runs):
+        self.player = player
+        self.runs = runs
--- cricket-score/test/model_test.py
+++ cricket-score/test/model_test.py
@@ -2,6 +2,8 @@
 """
 import unittest
 from score_model import Team
+from score_model import Player
+from score_model import Ball
 
 
 class TestTeam(unittest.TestCase):
@@ -9,11 +11,71 @@ class TestTeam(unittest.TestCase):
     Test functionality of the cricket team
     """
 
-    def testCreation(self):
-        team = Team()
-        self.assertEqual('', team.name)
+    def setUp(self):
+        self.team_name = 'The Team'
+        self.team = Team(self.team_name)
+        self.player_name = 'A Player'
+        self.player = Player(self.player_name)
+        self.another_player_name = 'Another Player'
+        self.another_player = Player(self.another_player_name)
+
+    def test_creation(self):
+        otago_name = 'Otago'
+        team = Team(otago_name)
+        self.assertEqual(otago_name, team.name)
+        self.assertEqual(0, team.num_players)
+
+    def test_adding_player(self):
+        self.assertEqual(0, self.team.num_players)
+        self.team.add_player(self.player)
+        self.assertEqual(1, self.team.num_players)
+
+    def test_adding_2_player(self):
+        self.assertEqual(0, self.team.num_players)
+        self.team.add_player(self.player)
+        self.team.add_player(self.another_player)
+        self.assertEqual(2, self.team.num_players)
+
+    def test_removing_2_player(self):
+        self.assertEqual(0, self.team.num_players)
+        self.team.add_player(self.player)
+        self.team.add_player(self.another_player)
+        self.team.remove_player(self.player)
+        self.team.remove_player(self.another_player)
+        self.assertEqual(0, self.team.num_players)
+
+class TestPlayer(unittest.TestCase):
+    """
+    Test functionality of the cricket player
+    """
+
+    def test_creation(self):
+        player_name = 'A Player'
+        player_ident = '20'
+        player = Player(player_name)
+        player.identifier = player_ident
+        self.assertEqual(player_name,player.name)
+        self.assertEqual(player_ident, player.identifier)
+
+    def test_creation_with_ident(self):
+        player_name = 'A Player'
+        player_ident = '20'
+        player = Player(player_name, player_ident)
+        self.assertEqual(player_name, player.name)
+        self.assertEqual(player_ident, player.identifier)
 
 
+class BallTest(unittest.TestCase):
+    """
+    Test functionality of a ball being bowled
+    """
+
+    def setUp(self):
+        self.player = Player('A Player')
+
+    def test_creation(self):
+        ball = Ball(self.player,0)
+        self.assertEqual(0,ball.runs)
 
 if __name__ == "__main__":
     # import sys;sys.argv = ['', 'Test.testTeam']
